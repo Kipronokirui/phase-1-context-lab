9521d8e3c8ea281a990d654b825b7bcde6fe380a
Kipronokirui <victorkipronokirui2@gmail.com>
2024-01-11 19:40:15 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
 /* Your Code Here */
+function createEmployeeRecord(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
 
+}
+const createEmployeeRecords = (row)=>{
+   return row.map(createEmployeeRecord); 
+}
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    this.timeInEvents.push({
+         type: "TimeIn",
+          date, 
+          hour: parseInt(hour, 10) });
+    return this;
+return this;
+}
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    this.timeOutEvents.push({ 
+        type: "TimeOut", 
+        date,
+         hour: parseInt(hour, 10) });
+    return this;
+}
+function hoursWorkedOnDate(dateAndHours){
+    const timeIn = this.timeInEvents.find(timeStamp => timeStamp.date === dateAndHours).hour
+    const timeOut = this.timeOutEvents.find(timeStamp => timeStamp.date === dateAndHours).hour
+    const totalHours = (timeOut - timeIn)
+    return totalHours / 100
+}
+function wagesEarnedOnDate(dateAndHours){
+    const wagedHours = hoursWorkedOnDate.call(this, dateAndHours);
+    const employeeWage = wagedHours * this.payPerHour;
+    return parseFloat(employeeWage.toString());
+}
+function  findEmployeeByFirstName(empArray,firstName){
+    return empArray.find((employee) =>{
+        return employee.firstName === firstName;
+    });
+}
+function calculatePayroll(arrayEmployees) {
+    return arrayEmployees.reduce((acc,rec) => {
+        return acc + allWagesFor.call(rec)
+    }, 0);
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +71,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
